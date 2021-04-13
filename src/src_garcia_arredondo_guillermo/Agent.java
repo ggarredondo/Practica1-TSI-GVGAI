f978922f5e9d21fbe8ce51c436087d931dced00f
ggarredondo <ggarredondo@correo.ugr.es>
2021-04-13 19:40:58 +0200

    Listo para empezar A*

--- src/src_garcia_arredondo_guillermo/Agent.java
+++ src/src_garcia_arredondo_guillermo/Agent.java
@@ -1,9 +1,11 @@
 package src_garcia_arredondo_guillermo;
 
+import core.game.Observation;
 import core.game.StateObservation;
 import core.player.AbstractPlayer;
 import ontology.Types;
 import tools.ElapsedCpuTimer;
+import tools.Vector2d;
 
 import java.util.ArrayList;
 import java.util.PriorityQueue;
@@ -14,15 +16,26 @@ public class Agent extends AbstractPlayer {
 
     public Agent(StateObservation stateObs, ElapsedCpuTimer elapsedTimer) {
         secuencia = new Stack<>();
+        Vector2d fescala = new Vector2d(stateObs.getWorldDimension().width/stateObs.getObservationGrid().length, stateObs.getWorldDimension().height/stateObs.getObservationGrid()[0].length);
+        Vector2d inicio = new Vector2d(Math.floor(stateObs.getAvatarPosition().x/fescala.x), Math.floor(stateObs.getAvatarPosition().y/fescala.y));
+        Vector2d portal = stateObs.getPortalsPositions(stateObs.getAvatarPosition())[0].get(0).position;
+        Vector2d objetivo = new Vector2d(Math.floor(portal.x/fescala.x), Math.floor(portal.y/fescala.y));
+        AEstrella(stateObs, inicio, objetivo);
     }
 
     public Types.ACTIONS act(StateObservation stateObs, ElapsedCpuTimer elapsedTimer) {
         return Types.ACTIONS.ACTION_NIL;
     }
 
-    public void AEstrella(StateObservation stateObs)
+    public int distanciaManhattam(int x1, int x2, int y1, int y2) {
+        return Math.abs(x1-x2) + Math.abs(y1-y2);
+    }
+
+    public void AEstrella(StateObservation stateObs, Vector2d inicio, Vector2d objetivo)
     {
         PriorityQueue<Nodo> abiertos = new PriorityQueue<>();
         ArrayList<Nodo> cerrados = new ArrayList<>();
+
+        //while (!abiertos.isEmpty() && )
     }
 }
--- src/src_garcia_arredondo_guillermo/Nodo.java
+++ src/src_garcia_arredondo_guillermo/Nodo.java
@@ -1,14 +1,15 @@
 package src_garcia_arredondo_guillermo;
 
 import ontology.Types;
+import tools.Vector2d;
 
 public class Nodo implements Comparable<Nodo> {
-    private int f, g, h;
-    private int x, y;
+    private int fil, col; // posición en el mapa
+    private int f, g, h; // coste y heurística
     private Nodo padre;
-    private Types.ACTIONS accion;
+    private Types.ACTIONS accion; // acción para llegar al nodo
 
-    public Nodo(int g, int h, Nodo padre) {
+    public Nodo(int fil, int col, int g, int h, Nodo padre) {
         this.g = g;
         this.h = h;
         this.padre = padre;
@@ -17,5 +18,9 @@ public class Nodo implements Comparable<Nodo> {
 
     public int compareTo(Nodo n) {
         return this.f - n.f;
+    }
+
+    public boolean equals(Vector2d v) {
+        return (this.fil == v.x) && (this.col == v.y);
     }
 }
